4c28f9dfad768238f22f9bae4d416d4b385576cd
Thomas Trotter <140984323+Climber1705@users.noreply.github.com>
2025-03-24 23:47:33 +0000

    Update game.py

--- src/game.py
+++ src/game.py
@@ -6,119 +6,111 @@ from minimax import MiniMax
 from random import randint
 
 """
-    The Game class
-    The class that represents the game
+    The Game class.
+    The class that represents the game.
 """
-
 class Game:
 
     """
-        Attributes of the Game class
+        Attributes of the Game class.
     """
-
     WIDTH : int = 7
-    HEIGTH : int = 6
-    # Max depth of the search of 7 or take some time to compute for higher values
-    SEARCH_DEPTH : int = 7
+    HEIGHT : int = 6
+    # Max depth of the search of 7 or take some time to compute for higher values.
+    DEPTH : int = 7
 
     board: Board
-    playersDisc: int
-    computerDisc: int
-    miniMax: MiniMax
-
-    """
-        Constructor of the Game class
-    """
+    player_disc: int
+    computer_disc: int
+    minimax: MiniMax
 
     def __init__(self):
-        self.playersDisc = 1
-        self.computerDisc = 2
+        self.player_disc = 1
+        self.computer_disc = 2
 
-        self.board = Board(np.zeros((self.HEIGTH, self.WIDTH), dtype=np.uint8), self.playersDisc, self.computerDisc)
-        self.miniMax = MiniMax(self.playersDisc, self.computerDisc)
+        self.board = Board(np.zeros((self.HEIGHT, self.WIDTH), dtype=np.uint8), self.player_disc, self.computer_disc)
+        self.minimax = MiniMax(self.player_disc, self.computer_disc)
 
     """
-        Gets the starting disc to determine who starts
-        @return: the starting disc
+        Gets the starting disc to determine who starts.
+        @return: the starting disc.
     """
-
-    def getStartingDisc(self) -> int:
+    def get_starting_disc(self) -> int:
         if randint(0, 1) == 0:
-            return self.playersDisc
-        return self.computerDisc
+            return self.player_disc
+        return self.computer_disc
 
     """
-        Gets the player's input
+        Gets the player's input.
+        @return: the player's input.
     """
-
-    def getPlayerInput(self):
+    def get_player_input(self) -> int:
         while True:
-            playerInput = input("Enter a column number: ")
-            if not playerInput.isdigit():
+            player_input = input("Enter a column number: ")
+            if not player_input.isdigit():
                 print("Invalid input. Please enter a number.")
                 continue
-            if int(playerInput) < 0 or int(playerInput) > 6:
+            if int(player_input) < 0 or int(player_input) > 6:
                 print("Invalid input. Please enter a number between 0 and 6.")
                 continue
-            if not self.board.isValidMove(int(playerInput)):
+            if not self.board.is_valid_move(int(player_input)):
                 print("Invalid input. Please enter a number for a column that is not full.")
                 continue
-            print("Player's move: ", playerInput)
-            return int(playerInput)
+            print("Player's move: ", player_input)
+            return int(player_input)
 
     """
-        Gets the computer's move
+        Gets the computer's move.
+        @return: the computer's move.
     """
-
-    def computerMove(self):
-        move, score = self.miniMax.miniMax(self.board, -float('inf'), float('inf'), self.SEARCH_DEPTH, self.computerDisc)
-        print("Computer's move: ", move)
+    def computer_move(self) -> int:
+        move, score = self.minimax.mini_max(self.board, -float('inf'), float('inf'), self.DEPTH, self.computer_disc)
+        print(f"Computer's move: {move}, Computer's score: {score}")
         return move
 
 
     """
-        Changes the disc after each turn
-        @param token: the current disc
-        @return: the new disc
+        Changes the disc after each turn.
+        @param token: the current disc.
+        @return: the new disc.
     """
-    def changeDisc(self, dics: int) -> int:
-        if dics == 1:
+    def change_disc(self, disc: int) -> int:
+        if disc == 1:
             return 2
         else:
             return 1
 
     """
-        Runs the game
+        Runs the game.
     """
-
     def run(self):
         running = True
-        disc = self.getStartingDisc()
-        print("Player's disc: ", self.playersDisc)
-        print("Computer's disc: ", self.computerDisc)
-        print("Player starts!" if disc == self.playersDisc else "Computer starts!")
+        disc = self.get_starting_disc()
+        print("Player's disc: ", self.player_disc)
+        print("Computer's disc: ", self.computer_disc)
+        print("Player starts!" if disc == self.player_disc else "Computer starts!")
         while running:
             self.board.display()
-            if disc == self.playersDisc:
-                move = self.getPlayerInput()
-                self.board.dropDisc(move, disc)
+            if disc == self.player_disc:
+                move = self.get_player_input()
+                self.board.drop_disc(move, disc)
                 print("=====================================")
             else:
-                move = self.computerMove()
-                self.board.dropDisc(move, disc)
+                move = self.computer_move()
+                self.board.drop_disc(move, disc)
                 print("=====================================")
-            if self.board.isWinner(self.playersDisc):
+            if self.board.is_winner(self.player_disc):
                 self.board.display()
                 print("Player wins!")
                 running = False
-            elif self.board.isWinner(self.computerDisc):
+            elif self.board.is_winner(self.computer_disc):
                 self.board.display()
                 print("Computer wins!")
                 running = False
-            elif self.board.isBoardFull():
+            elif self.board.is_board_full():
                 self.board.display()
                 print("It's a tie!")
                 running = False
             else:
-                disc = self.changeDisc(disc)
+                disc = self.change_disc(disc)
         print("Thank you for playing!")
